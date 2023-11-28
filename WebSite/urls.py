d28cbe6d61a3981d3a90b3fc5b00a015b6e1cfd4
unknown <svytsop@gmail.com>
2023-11-28 22:34:56 +0200

    Add_Mongo_DB

--- WebSite/urls.py
+++ WebSite/urls.py
@@ -17,7 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Swagger Api",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
